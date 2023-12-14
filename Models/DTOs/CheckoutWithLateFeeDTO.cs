0a87fe628da2fab230dceb7bccfe5623d10ce43c
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-14 11:03:30 -0600

    ready for adding react client

--- Models/DTOs/CheckoutWithLateFeeDTO.cs
+++ Models/DTOs/CheckoutWithLateFeeDTO.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using Loncotes.Models;
 
 public class CheckoutWithLateFeeDTO 
 {
--- Models/Patron.cs
+++ Models/Patron.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-
 namespace Loncotes.Models;
 
 public class Patron 
